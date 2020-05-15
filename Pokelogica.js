e39e94c4c4feb0bdfe234487749e4e5eca7400b5
Willian Candanoza <candanoza170@gmail.com>
2020-05-15 11:03:41 -0500

    nuevo commit badge

--- Pokelogica.js
+++ Pokelogica.js
@@ -4,7 +4,7 @@ var nombrePokemon         =   document.getElementById("nombrePokemon").value;
 var habilidadesPokemon    =  document.getElementById("habilidadesPokemon");
 var tipoCambio    =  document.getElementById("tipoCambio");
 var tipo   =  document.getElementById("tipo");
-
+var buttomSearch = document.getElementById("mePona");
 
 
 nombrePokemon  = nombrePokemon.toLowerCase();
@@ -15,36 +15,28 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+nombrePokemon+'/')
     return response.json();
   })
   .then(function(neko) {
-    console.log(neko.abilities['0'].ability.name);
+/*     console.log(neko.abilities['0'].ability.name);
     console.log(neko.abilities['1'].ability.name);
     console.log(neko.sprites.front_default);
     console.log(neko.forms['0'].name);
     console.log(neko.types['0'].type.name);
-
+ */
     imagen_pokemon.src = neko.sprites.front_default;
     mePona.innerHTML=' <p> Nombre: '+neko.forms['0'].name+' </p> <hr>' ; 
     habilidadesPokemon.innerHTML=  ' <p> Habilidad 1: '+neko.abilities['0'].ability.name+' <br>Habilidad 2: '+neko.abilities['1'].ability.name+' </p>';
-    tipo.innerHTML=' <p> <b> Tipo: </b>'+neko.types['0'].type.name+'</p>';
-    tipoCambio.innerHTML= neko.types['0'].type.name ;
     
-   
+    
 
-    if ( neko.types['0'].type.name == "normal"  ) {
-
-      tipoCambio.classList.remove('btn btn-warning');
-      $("#tipoCambio").addClass("btn btn-light"); /* */
-
-    } else if  (neko.types['0'].type.name == "electric" ){
-
-      
-      $("#tipoCambio").addClass("btn btn-info");
-
-    } else if  (neko.types['0'].type.name == "fire") {
-      $("#tipoCambio").addClass("btn btn-danger");
-
-    } else {
-      console.log("error no hay tipo");
+    var type = neko.types['0'].type.name;
+    if(type == 'electric'){
+      tipo.innerHTML='<b>Tipo: </b><span class="badge badge-warning">'+neko.types['0'].type.name+'</span>';
     }
+
+   
+    
+    
+    console.log(tipo);
+    
   
   }); 
 }
