871d5348516ae6fa72cc868cc12215093668afc9
Marcos <marcosj465@gmail.com>
2020-05-14 22:32:35 -0400

    cambios_desde_0

---
+++ Pokelogica.js
@@ -0,0 +1,28 @@
+function buscaPuchamon() {
+var imagen_pokemon  = document.getElementById("imagenpokemon");
+var nombrePokemon   =document.getElementById("nombrePokemon").value;
+console.log(nombrePokemon);
+var habilidadesPokemon   =document.getElementById("habilidadesPokemon");
+
+nombrePokemon  = nombrePokemon.toLowerCase();
+fetch('https://pokeapi.co/api/v2/pokemon/'+nombrePokemon+'/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(neko) {
+    console.log(neko.abilities['0'].ability.name);
+    console.log(neko.abilities['1'].ability.name);
+    console.log(neko.sprites.front_default);
+    console.log(neko.forms['0'].name);
+
+    imagen_pokemon.src = neko.sprites.front_default;
+    nombrePokemon.innerHTML=' <p> Nombre: '+neko.forms['0'].name+' </p> <hr>' ; 
+    habilidadesPokemon.innerHTML=  ' <p> Habilidad 1: '+neko.abilities['0'].ability.name+' <br>Habilidad 2: '+neko.abilities['1'].ability.name+' </p>';
+
+  }); 
+}
+
+  
+  
+  
+  
