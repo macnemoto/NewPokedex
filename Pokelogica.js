035a37e3a4d38d06c7be13e148f17d36bcffeeb1
marcos pirela <marcosj465@gmail.com>
2020-05-15 02:51:08 -0400

    Implementacion tipos

--- Pokelogica.js
+++ Pokelogica.js
@@ -1,9 +1,8 @@
-
-
 function buscaPuchamon() {
 var imagen_pokemon        =   document.getElementById("imagenpokemon");
 var nombrePokemon         =   document.getElementById("nombrePokemon").value;
 var habilidadesPokemon    =  document.getElementById("habilidadesPokemon");
+var tipo   =  document.getElementById("tipo");
 nombrePokemon  = nombrePokemon.toLowerCase();
 fetch('https://pokeapi.co/api/v2/pokemon/'+nombrePokemon+'/')
   .then(function(response) {
@@ -14,11 +13,12 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+nombrePokemon+'/')
     console.log(neko.abilities['1'].ability.name);
     console.log(neko.sprites.front_default);
     console.log(neko.forms['0'].name);
+    console.log(neko.types['0'].type.name);
 
     imagen_pokemon.src = neko.sprites.front_default;
     mePona.innerHTML=' <p> Nombre: '+neko.forms['0'].name+' </p> <hr>' ; 
     habilidadesPokemon.innerHTML=  ' <p> Habilidad 1: '+neko.abilities['0'].ability.name+' <br>Habilidad 2: '+neko.abilities['1'].ability.name+' </p>';
-    
+    tipo.innerHTML=' <p> <b> Tipo: </b>'+neko.types['0'].type.name+'</p>';
   }); 
 }
 
