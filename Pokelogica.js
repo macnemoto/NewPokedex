f09327081bdaa1b5a635983b5e1f06e138d68b51
marcos pirela <marcosj465@gmail.com>
2020-05-15 07:23:57 -0400

    Tratando de poner boton de colores

--- Pokelogica.js
+++ Pokelogica.js
@@ -2,8 +2,14 @@ function buscaPuchamon() {
 var imagen_pokemon        =   document.getElementById("imagenpokemon");
 var nombrePokemon         =   document.getElementById("nombrePokemon").value;
 var habilidadesPokemon    =  document.getElementById("habilidadesPokemon");
-var tipo   =  document.getElementById("tipo");
+var tipoCambio            =  document.getElementById("tipoCambio");
+var tipo                  =  document.getElementById("tipo");
+
+
+
 nombrePokemon  = nombrePokemon.toLowerCase();
+
+
 fetch('https://pokeapi.co/api/v2/pokemon/'+nombrePokemon+'/')
   .then(function(response) {
     return response.json();
@@ -19,8 +25,31 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+nombrePokemon+'/')
     mePona.innerHTML=' <p> Nombre: '+neko.forms['0'].name+' </p> <hr>' ; 
     habilidadesPokemon.innerHTML=  ' <p> Habilidad 1: '+neko.abilities['0'].ability.name+' <br>Habilidad 2: '+neko.abilities['1'].ability.name+' </p>';
     tipo.innerHTML=' <p> <b> Tipo: </b>'+neko.types['0'].type.name+'</p>';
+    tipoCambio.innerHTML= neko.types['0'].type.name ;
+    
+   
+
+    if ( neko.types['0'].type.name == "normal"  ) {
+
+      tipoCambio.classList.remove('btn btn-warning');
+      tipoCambio.classList.add('btn btn-primary');
+     /*  $("#tipoCambio").addClass("btn btn-light"); */
+
+    } else if  (neko.types['0'].type.name == "electric" ){
+
+      
+
+
+    } else if  (neko.types['0'].type.name == "fire") {
+   
+
+    } else {
+      console.log("error no hay tipo");
+    }
+  
   }); 
 }
+
 
   
   
