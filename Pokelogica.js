ef96d7b7587a5ea19924c2e3804da0a6c2077ec8
Marcos <marcosj465@gmail.com>
2020-05-17 12:06:16 -0400

    Cambios a los tiepos monstar tipo 1 y tipo 2 correcta mente

--- Pokelogica.js
+++ Pokelogica.js
@@ -42,71 +42,74 @@ function buscaPuchamon() {
     habilidadesPokemon.innerHTML = all_habilidades;
 
 
+    
 
 
-
-
+    var itypes    = 1;
       var typesArrayPokemon = neko.types;
       var all_types          = '';
       typesArrayPokemon.forEach(obj => {
         if (obj.type.name == 'electric') {
-          all_types += '<b>Tipo: </b><span class="badge badge-warning">' + obj.type.name + '</span><br>';
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-warning">' + obj.type.name + '</span><br>';
+          itypes++;
         } else if (obj.type.name == 'normal') {
-          all_types += '<b>Tipo: </b><span class="badge badge-light">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-light">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'fire') {
-          all_types += '<b>Tipo: </b><span class="badge badge-danger">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-danger">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'ice') {
-          all_types += '<b>Tipo: </b><span class="badge badge-info">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-info">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'grass') {
-          all_types += '<b>Tipo: </b><span class="badge badge-success">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-success">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'grass') {
-          all_types += '<b>Tipo: </b><span class="badge badge-success">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-success">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'dark') {
-          all_types += '<b>Tipo: </b><span class="badge badge-secondary">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-secondary">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'water') {
-          all_types += '<b>Tipo: </b><span class="badge badge-primary">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-primary">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'ghost') {
-          all_types += '<b>Tipo: </b><span class="badge badge-fantama">' + obj.type.name + '</span><br>';
+          all_types += '<b>itypes++;Tipo'+itypes+': </b><span class="badge badge-fantama">' + obj.type.name + '</span><br>';
 
         } else if (obj.type.name == 'poison') {
-          all_types += '<b>Tipo: </b><span class="badge badge-poison">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-poison">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'rock') {
-          all_types += '<b>Tipo: </b><span class="badge badge-rock">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-rock">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'bug') {
-          all_types += '<b>Tipo: </b><span class="badge badge-bug">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-bug">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'steel') {
-          all_types += '<b>Tipo: </b><span class="badge badge-steel">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-steel">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'fairy') {
-          all_types += '<b>Tipo: </b><span class="badge badge-fairy">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-fairy">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'dragon') {
-          all_types += '<b>Tipo: </b><span class="badge badge-dragon">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-dragon">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'ground') {
-          all_types += '<b>Tipo: </b><span class="badge badge-ground">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-ground">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'flying') {
-          all_types += '<b>Tipo: </b><span class="badge badge-flying">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-flying">' + obj.type.name + '</span><br>';
+itypes++;
         } else if (obj.type.name == 'fighting') {
-          all_types += '<b>Tipo: </b><span class="badge badge-fighting">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-fighting">' + obj.type.name + '</span><br>';
+itypes++;
         }else if (obj.type.name == 'psychic') {
-          all_types += '<b>Tipo: </b><span class="badge badge-psychic">' + obj.type.name + '</span><br>';
-
+          all_types += '<b>Tipo'+itypes+': </b><span class="badge badge-psychic">' + obj.type.name + '</span><br>';
+itypes++;
         }
+        
       });
+      
       tipo.innerHTML = all_types;
     
 
